bf587e23647877305f59c1be4c7362eff557d9d5
niumandzi <amazheykin@gmail.com>
2024-11-18 06:18:02 +0300

    added test

--- calculate.py
+++ calculate.py
@@ -1,33 +1,61 @@
 import circle
 import square
 
+figs = {
+    'circle': {
+        'area': circle.area,
+        'perimeter': circle.perimeter
+    },
+    'square': {
+        'area': square.area,
+        'perimeter': square.perimeter
+    }
+}
 
-figs = ['circle', 'square']
-funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+    'area-circle': 1,
+    'perimeter-circle': 1,
+    'area-square': 1,
+    'perimeter-square': 1
+}
+
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    """
+    Вычисляет и выводит результат функции для заданной фигуры.
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    Параметры:
+    fig (str): Название фигуры ('circle' или 'square').
+    func (str): Название функции ('perimeter' или 'area').
+    size (list): Список размеров, необходимых для вычисления.
+
+    Пример вызова:
+    calc('circle', 'area', [5])
+    """
+    assert fig in figs, f"Unknown figure: {fig}"
+    assert func in figs[fig], f"Unknown function: {func}"
+    assert all(s > 0 for s in size), "All size parameters must be positive"
+
+    result = figs[fig][func](*size)
+    return result
+
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
+    """
+    Основная функция, которая запрашивает у пользователя данные и вызывает calc.
+    """
+    func = ''
+    fig = ''
+    size = []
 
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {list(figs.keys())}:\n")
 
+    while func not in figs[fig]:
+        func = input(f"Enter function name, available are {list(figs[fig].keys())}:\n")
 
+    while len(size) != sizes[f"{func}-{fig}"]:
+        size = list(map(int, input("Input figure sizes separated by space:\n").split()))
+
+    result = calc(fig, func, size)
+    print(f'{func} of {fig} is {result}')
